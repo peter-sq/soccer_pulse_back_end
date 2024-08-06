d356bff96110952f64e1538924b518e5c220f694
peter-sq <awe.petersq@gmail.com>
2024-08-06 15:12:37 +0100

    config file

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,19 @@
-// db.js
 import mongoose from 'mongoose';
-
 
 const connectDB = async () => {
   if (mongoose.connection.readyState === 0) {
-  try {
-    const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/soccer-predictions';
-    await mongoose.connect(uri);
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+    try {
+      const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/soccer-predictions';
+      console.log('Connecting to MongoDB with URI:', uri); // Log URI for debugging
+      await mongoose.connect(uri);
+      console.log('MongoDB connected');
+    } catch (err) {
+      console.error('Connection error:', err.message);
+      process.exit(1);
+    }
+  } else {
+    console.log('MongoDB already connected');
   }
-}
 };
 
 export default connectDB;
